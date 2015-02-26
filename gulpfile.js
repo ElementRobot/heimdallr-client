b609b62b47e723acf3ca595c8a7b6a1bc1a0361c
jf <jf@elementrobot.com>
2015-02-25 19:33:45 -0800

    Updating bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,19 @@ var buffer = require('vinyl-buffer');
 var watchify = require('watchify');
 var browserify = require('browserify');
 
-var bundler = watchify(browserify('./src/heimdallr-client.js', watchify.args));
+// Browserify
+var bundler = watchify(browserify('./src/heimdallr-client.js', watchify.args).exclude('socket.io-client'));
 // add any other browserify options or transforms here
 bundler.transform('brfs');
 
-gulp.task('js', bundle); // so you can run `gulp js` to build the file
+gulp.task('default', bundle);
 bundler.on('update', bundle); // on any dep update, runs the bundler
 
 function bundle() {
   return bundler.bundle()
     // log errors if they happen
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('bundle.js'))
+    .pipe(source('heimdallr-client.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
     .pipe(sourcemaps.write('./')) // writes .map file
