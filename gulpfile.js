90983c4dc3df82455664929e9e8843907bddd397
jf <jf@elementrobot.com>
2015-02-28 11:53:26 -0800

    Added updating github pages index file to docs task.

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var gulp = require('gulp'),
     fs = require('fs'),
     path = require('path'),
     exec = require('child_process').exec,
+    replace = require('replace'),
     meta = require('./package.json'),
     jsdocConfPath = './docs/conf.json',
     jsdocConf = require(jsdocConfPath),
@@ -42,15 +43,26 @@ gulp.task('js', function js() {
 });
 
 gulp.task('docs', function docs(cb) {
+    var destination = path.join('docs', meta.name, meta.version);
+
     // Unfortunately jsdoc doesn't fit well into the gulp paradigm since
     // it only provides a CLI tool.
-    jsdocConf.opts.destination = path.join('./docs', meta.name, meta.version);
+    jsdocConf.opts.destination = destination;
     jsdocConfStr = JSON.stringify(jsdocConf, null, 2);
     fs.writeFileSync(jsdocConfPath, jsdocConfStr);
     exec('./node_modules/.bin/jsdoc -c ./docs/conf.json -p', function(err, stdout){
         if(stdout) console.log(stdout);
         if(err) return cb(err);
         cb();
+    });
+
+    // Update the github-pages redirect
+    replace({
+        regex: '\'.*\'',
+        replacement: '\'' + destination + '/index.html\'',
+        paths: ['index.html'],
+        recurse: false,
+        silent: true
     });
 });
 
